60a7d4c4aacaeb3f4ae3f0ede3f65e415225d265
Vlastimir Radojevic <vlastimirsd@gmail.com>
2020-08-26 08:01:07 +0200

    Initial Commit

---
+++ SwiftUIAdjustment/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  SwiftUIAdjustment
+//
+//  Created by Vlastimir on 26/08/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIAdjustment/SwiftUIAdjustmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAdjustmentApp.swift
+//  SwiftUIAdjustment
+//
+//  Created by Vlastimir on 26/08/2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAdjustmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
